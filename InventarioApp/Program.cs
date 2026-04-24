43db69479a5ce551bef19be21028e18c17e06fed
Jean C Murillo <jeanca1229@gmail.com>
2026-04-24 10:19:28 -0500

    Primer push con estructura base del proyecto

---
+++ InventarioApp/Program.cs
@@ -0,0 +1,31 @@
+﻿// ============================================================
+// SISTEMA DE INVENTARIO - Clase 1.1
+// Estado: Mensaje de bienvenida
+// ============================================================
+
+using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+Console.WriteLine("==========================================");
+Console.WriteLine("    SISTEMA DE GESTIÓN DE INVENTARIO      ");
+Console.WriteLine("==========================================");
+Console.WriteLine();
+Console.WriteLine($"version: {version}");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+Console.WriteLine($".NET Version: {Environment.Version}");
+Console.WriteLine();
+Console.WriteLine("Estructura del proyecto");
+Console.WriteLine("Configuracion .csproj");
+Console.WriteLine("Carpeta src/ creada");
+Console.WriteLine("Metadatos configurados");
+Console.WriteLine();
+Console.WriteLine("Proximo paso");
+
+
+
+
+
+
+
+
